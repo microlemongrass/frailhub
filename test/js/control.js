938e7714e8c116d45b222778e8bb403fd7daad39
microlemongrass <37034207+microlemongrass@users.noreply.github.com>
2020-07-14 23:06:42 +0900

    Create control.js

---
+++ test/js/control.js
@@ -0,0 +1,27 @@
+// ボタンを押すと、ブロック内のプログラムが実行される
+var video = document.getElementById('video');
+var layout = document.getElementById('layout');
+
+$('#control_positionX').on("input", function() {
+    video.style.left = control_positionX.value + "px";
+});
+
+$('#control_positionY').on("input", function() {
+    video.style.top = control_positionY.value + "px";
+});
+
+$('#control_width').on("input", function() {
+    video.style.width = control_width.value + "px";
+});
+
+$('#control_height').on("input", function() {
+    video.style.height = control_height.value + "px";
+});
+
+$('#frame_width').on("input", function() {
+    layout.style.width = frame_width.value + "px";
+});
+
+$('#frame_height').on("input", function() {
+    layout.style.height = frame_height.value + "px";
+});
